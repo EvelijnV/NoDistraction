f4400c82078becc49327376e6bc52058e1f07be4
EvelijnV <evelijnv@live.nl>
2018-03-07 11:02:11 +0100

    first test

---
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nodistracion.evelijn.nodistraction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
