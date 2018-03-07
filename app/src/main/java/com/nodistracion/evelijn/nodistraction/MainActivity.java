1131bffcd86d820f88aea64bdaa9a741b90d5048
EvelijnV <evelijnv@live.nl>
2018-03-07 15:11:08 +0100

    test activity

--- app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // I want this activity to be in the AppListTest branch
     }
 }
