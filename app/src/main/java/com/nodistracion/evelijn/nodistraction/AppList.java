739c20323f8b29ab6fa1e02212fccdc084ee399f
EvelijnV <evelijnv@live.nl>
2018-03-07 15:53:06 +0100

    test activity

---
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/AppList.java
@@ -0,0 +1,26 @@
+package com.nodistracion.evelijn.nodistraction;
+
+/**
+ * Created by Evelijn on 7-3-2018.
+ */
+
+import android.graphics.drawable.Drawable;
+
+public class AppList {
+
+    private String name;
+    Drawable icon;
+
+    public AppList(String name, Drawable icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Drawable getIcon() {
+        return icon;
+    }
+}
--- app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
@@ -1,15 +1,65 @@
 package com.nodistracion.evelijn.nodistraction;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // I want this activity to be in the AppListTest branch
+        ListView userInstalledApps = (ListView)findViewById(R.id.installed_app_list);
+
+        List<AppList> installedApps = getInstalledApps();
+        AppAdapter installedAppAdapter = new AppAdapter(MainActivity.this, installedApps);
+        userInstalledApps.setAdapter(installedAppAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<AppList> getInstalledApps() {
+        List<AppList> res = new ArrayList<AppList>();
+        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+        for (int i = 0; i < packs.size(); i++) {
+            PackageInfo p = packs.get(i);
+            if ((isSystemPackage(p) == false)) {
+                String appName = p.applicationInfo.loadLabel(getPackageManager()).toString();
+                Drawable icon = p.applicationInfo.loadIcon(getPackageManager());
+                res.add(new AppList(appName, icon));
+            }
+        }
+        return res;
+    }
+
+    private boolean isSystemPackage(PackageInfo pkgInfo) {
+        return ((pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) ? true : false;
     }
 }
