cc145fe6cfbc13bb75f9d89d0c714d4a50b1cb4a
EvelijnV <evelijnv@live.nl>
2018-03-09 10:21:30 +0100

    test activity

--- app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //this is a test line
-        //ok omdat ij het zegt
+        
     }
 }
