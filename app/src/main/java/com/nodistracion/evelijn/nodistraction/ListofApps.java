2cd9af6b1f8962fb50261cba4e286a91818f7229
Cassey <cassey.k@hotmail.com>
2018-03-14 14:51:04 +0100

    layout

--- app/src/main/java/com/nodistracion/evelijn/nodistraction/ListofApps.java
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/ListofApps.java
@@ -20,7 +20,7 @@ public class ListofApps extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listof_apps);
+        setContentView(R.layout.listapps);
         //Typeface myTypeface = Typeface.createFromAsset(getAssets(), "fonts/bebasneue.otf");
         //TextView myTextview=findViewById(R.id.installed_app_list);
        // myTextview.setTypeface(myTypeface);
@@ -52,11 +52,11 @@ public class ListofApps extends Activity {
 
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
