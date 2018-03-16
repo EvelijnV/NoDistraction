d540c2a49d140568131ca0dc91afb872f52ef846
Cassey <cassey.k@hotmail.com>
2018-03-16 13:17:44 +0100

    layout works with app list

--- app/src/main/java/com/nodistracion/evelijn/nodistraction/ListofApps.java
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/ListofApps.java
@@ -1,12 +1,20 @@
 package com.nodistracion.evelijn.nodistraction;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
-import java.util.Timer;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Cassey on 16/03/2018.
@@ -21,7 +29,71 @@ public class ListofApps extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listapps);
         timebutton=(Button)findViewById((R.id.timebutton));
+
+
+        ListView userInstalledApps = (ListView)findViewById(R.id.installed_app_list);
+
+        List<AppList> installedApps = getInstalledApps();
+        AppAdapter installedAppAdapter = new AppAdapter(ListofApps.this, installedApps);
+        userInstalledApps.setAdapter(installedAppAdapter);
+
+
+
+    //this piece below, can't seem to solve it, but it should block apps
+       /* ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        List l = am.getRecentTasks(1, ActivityManager.RECENT_WITH_EXCLUDED);
+        Iterator i = l.iterator();
+        PackageManager pm = this.getPackageManager();
+        while (i.hasNext()) {
+            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo)(i.next());
+            try {
+                CharSequence c = pm.getApplicationLabel(pm.getApplicationInfo(
+                        info.processName, PackageManager.GET_META_DATA));
+                Log.w("LABEL", c.toString());
+                //created the xml blockmessage to show up when an app is opened, perhaps should be used on the place of LABEL?
+            } catch (Exception e) {
+// Name Not Found Exception
+            }
+        }*/
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<AppList> getInstalledApps() {
+        List<AppList> res = new ArrayList<AppList>();
+        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+        for (int i = 0; i < packs.size(); i++) {
+            PackageInfo p = packs.get(i);
+            String appName = p.applicationInfo.loadLabel(getPackageManager()).toString();
+                Drawable icon = p.applicationInfo.loadIcon(getPackageManager());
+                res.add(new AppList(appName, icon));
+
+        }
+        return res;
+    }
+
+    private boolean isSystemPackage(PackageInfo pkgInfo) {
+        return ((pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) ? true : false;
+    }
+
+
 
     public void TimerClicked (View view) {
         {Intent intent=new Intent(this,
