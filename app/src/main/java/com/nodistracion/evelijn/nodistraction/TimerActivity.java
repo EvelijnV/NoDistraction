79c3619c95c9ec143be984ce7a61aa2de15f95e6
EvelijnV <evelijnv@live.nl>
2018-03-16 14:38:06 +0100

    time saved

--- app/src/main/java/com/nodistracion/evelijn/nodistraction/TimerActivity.java
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/TimerActivity.java
@@ -1,10 +1,16 @@
 package com.nodistracion.evelijn.nodistraction;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 /**
  * Created by Cassey on 16/03/2018.
@@ -14,17 +20,24 @@ public class TimerActivity extends AppCompatActivity{
 
 
     private Button settimebutton;
+    private TimePicker timepicker;
+    public int selectedHour;
+    public int selectedMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-        settimebutton=(Button)findViewById((R.id.settimebutton));
+        settimebutton = findViewById((R.id.settimebutton));
+        timepicker = findViewById(R.id.timePicker);
+        timepicker.setIs24HourView(true);
+
     }
 
     public void onClickSet (View view) {
-        {Intent intent=new Intent(this,
-                ItemsActivity.class);
-            startActivity(intent);}
+        selectedHour = timepicker.getHour();
+        selectedMinute = timepicker.getMinute();
+        Intent intent = new Intent(this, ItemsActivity.class);
+            startActivity(intent);
     }
 }
