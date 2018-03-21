012bfd5bd7ffa3d3baf019affb7104fe49e39e01
Cassey <cassey.k@hotmail.com>
2018-03-21 11:56:28 +0100

    layout works with not working checkbox and progressbar

--- app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
+++ app/src/main/java/com/nodistracion/evelijn/nodistraction/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 
 import com.nodistracion.evelijn.nodistraction.ListofApps.ListofApps;
@@ -14,6 +15,7 @@ import com.nodistracion.evelijn.nodistraction.ListofApps.ListofApps;
 public class MainActivity extends AppCompatActivity {
     private int interactionMode;
     private Button YourApps;
+    private ProgressBar Load;
 
 
 
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         YourApps=(Button)findViewById((R.id.button));
+        Load=(ProgressBar)findViewById(R.id.loading);
+        Load.setVisibility(View.INVISIBLE);
 
 
 
@@ -40,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }*/
    public void YourAppsClicked(View view) {
+       YourApps.setVisibility(View.INVISIBLE);
+       Load.setVisibility(View.VISIBLE);
+
       {Intent intent=new Intent(this,
                ListofApps.class);
        startActivity(intent);}
